819c8c1c930700c87367b8bce41879f405530b05
Toan1011 <147304482+Toan1011@users.noreply.github.com>
2024-03-01 17:06:48 +0700

    Add files via upload

---
+++ project23/db.php
@@ -0,0 +1,11 @@
+<?php
+    $users = [
+        [
+        "username" => "Toan",
+        "password" => password_hash("toan123", PASSWORD_DEFAULT), // Store hashed password
+        "name" => "Toan Pham",
+        "email" => "toan123@gmail.com"
+        ],
+       ];
+       
+?>
