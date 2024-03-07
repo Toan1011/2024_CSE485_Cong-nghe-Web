4370fdfca15aa80b5d114a6b2e2bfbb90e8d8859
Toan1011 <147304482+Toan1011@users.noreply.github.com>
2024-03-07 09:49:10 +0700

    Delete project22 directory

--- project22/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $user = [
-        'fullname' => 'Antonio Fenice',
-        'email' => 'anhnguyen988082@gmail.com',
-        'phonenumber' => '343-459-4350',
-        'companyname' => 'CanAmeria Realty',
-        'avatar' => 'img/1.jpg'
-       ];
-?>
