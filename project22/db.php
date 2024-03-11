bf675f10fa36f21d3e6c2cb5a697b68a053ed3e2
Toan1011 <phamtoann1011@gmail.com>
2024-03-11 07:05:10 +0700

    BTTH01 - Pham Quoc Toan

---
+++ project22/db.php
@@ -0,0 +1,9 @@
+<?php
+    $user = [
+        'fullname' => 'Antonio Fenice',
+        'email' => 'anhnguyen988082@gmail.com',
+        'phonenumber' => '343-459-4350',
+        'companyname' => 'CanAmeria Realty',
+        'avatar' => 'img/1.jpg'
+       ];
+?>
